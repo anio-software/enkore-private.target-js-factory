a95772388ce781ba2d1eda0f692f396c9dec7a1c
eXory2024 <exory@anio.software>
2024-09-17 23:18:52 +0200

    Add dummy initializeTarget.

--- src/realm-js/integration/index.mjs
+++ src/realm-js/integration/index.mjs
@@ -1,5 +1,13 @@
-export function initializeTarget() {
+import {
+	loadRealmDependencies
+} from "../auto/base-realm.mjs"
 
-	
+export async function initializeTarget() {
+	const {getDependency} = await loadRealmDependencies(
+		project_root, "realm-js"
+	)
 
+	const rollup = getDependency("rollup")
+
+	console.log("rollup is", rollup)
 }
