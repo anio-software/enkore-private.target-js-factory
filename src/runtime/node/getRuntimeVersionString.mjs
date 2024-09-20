9cd0db0f58cedc17947a7c0ee3f257c96e163221
eXory2024 <exory@anio.software>
2024-09-20 15:06:31 +0200

    Update src/runtime/node/getRuntimeVersionString.mjs.

--- src/runtime/node/getRuntimeVersionString.mjs
+++ src/runtime/node/getRuntimeVersionString.mjs
@@ -2,24 +2,13 @@ import path from "node:path"
 import readJSONFile from "./util/readJSONFile.mjs"
 import {createRequire} from "node:module"
 
-async function loadPackageJSONs(project_root) {
-	const require1 = createRequire(path.join(project_root, "index.js"))
-	const realm_package_json = require1.resolve("@4tune/realm-js-and-web-base/package.json")
-
-	const require2 = createRequire(realm_package_json)
-	const runtime_package_json = require2.resolve("@4tune/js-and-web-runtime/package.json")
-
-	return {
-		base: await readJSONFile(realm_package_json),
-		runtime: await readJSONFile(runtime_package_json)
-	}
-}
-
 export default async function(project_root) {
-	return ""
-	/*
-	const {base, runtime} = await loadPackageJSONs(project_root)
+	const require = createRequire(path.join(project_root, "index.js"))
+	const runtime_path = require.resolve("@fourtune/realm-js/package.json")
+	const runtime = await readJSONFile(runtime_path)
 
-	return `base ${base.version} @ runtime ${runtime.version}`
-	*/
+	const base_realm_version_path = require.resolve("@fourtune/realm-js/base-realm/version")
+	const {default: base_realm_version} = await import(base_realm_version_path)
+
+	return `v${runtime.version} [base v${base_realm_version}]`
 }
