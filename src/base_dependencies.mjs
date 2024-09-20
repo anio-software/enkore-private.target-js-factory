5bfebf4c93c8c6b01b2b3e943029f4f21b56b859
eXory2024 <exory@anio.software>
2024-09-20 15:27:19 +0200

    Add realm base dependency "@rollup/plugin-terser".

--- src/base_dependencies.mjs
+++ src/base_dependencies.mjs
@@ -7,6 +7,10 @@ export default {
 		version: "15.2.3",
 		import_code: `import nodeResolve from "@rollup/plugin-node-resolve"\nexport default nodeResolve`
 	},
+	"@rollup/plugin-terser": {
+		version: "0.4.4",
+		import_code: `import terser from "@rollup/plugin-terser"\nexport default terser`
+	},
 	"rollup-plugin-dts": {
 		version: "6.1.1",
 		import_code: `import {dts} from "rollup-plugin-dts"\nexport default dts`
