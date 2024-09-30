29ad239df5b1b470aea01819c539c92d3550bea0
eXory2024 <exory@anio.software>
2024-09-30 22:13:44 +0200

    Add dummy implementation for testing.

--- src/runtime/implementation/context/createDefaultContext.mjs
+++ src/runtime/implementation/context/createDefaultContext.mjs
@@ -1,3 +1,3 @@
 export default function(runtime, options_or_context) {
-	
+	return "create context return value"
 }
--- src/runtime/implementation/context/useContext.mjs
+++ src/runtime/implementation/context/useContext.mjs
@@ -1,3 +1,3 @@
 export default function(runtime, options_or_context) {
-	
+	return "use context return value"
 }
