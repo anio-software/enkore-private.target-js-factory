d3e98bd54bff764fa46b682d4f05f8ec343beb2c
eXory2024 <exory@anio.software>
2024-10-07 00:17:41 +0200

    Add instance.log[level] methods.

---
+++ src/runtime/implementation/getLogLevels.mjs
@@ -0,0 +1,12 @@
+//
+// make sure this matches with "types/LogLevelType.d.mts"
+//
+export default function() {
+	return {
+		"error": 3,
+		"warn" : 4,
+		"info" : 5,
+		"debug": 6,
+		"trace": 7
+	}
+}
