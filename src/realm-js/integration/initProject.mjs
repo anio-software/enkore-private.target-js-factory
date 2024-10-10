c78a3b7af937f54dda0fe34751844ede88684e05
eXory2024 <exory@anio.software>
2024-10-10 23:33:40 +0200

    Add empty initProject for type "async-sync".

--- src/realm-js/integration/initProject.mjs
+++ src/realm-js/integration/initProject.mjs
@@ -1,3 +1,5 @@
+import asyncSyncProjectInit from "./type/async-sync/initProject.mjs"
+
 export default async function(fourtune_session, writeFile) {
 	await writeFile(
 		"tsconfig.json",
@@ -14,4 +16,10 @@ export default async function(fourtune_session, writeFile) {
 	}
 }\n`, {overwrite:false}
 	)
+
+	const project_config = fourtune_session.getProjectConfig()
+
+	if (project_config.type === "async-sync") {
+		await asyncSyncProjectInit(fourtune_session, writeFile)
+	}
 }
