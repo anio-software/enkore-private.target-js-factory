25dbdb6a9ef671f1d33b4003bdb1a8ca0f1dade1
eXory2024 <exory@anio.software>
2024-10-10 23:33:28 +0200

    Implement init for async-sync.

--- src/realm-js/integration/type/async-sync/init.mjs
+++ src/realm-js/integration/type/async-sync/init.mjs
@@ -1,81 +1,30 @@
 export default async function(fourtune_session) {
 	const project_config = fourtune_session.getProjectConfig()
-	const target = project_config
+	const {target} = project_config
+	const {function_name} = target
+
+	const mapping = {}
+
+	mapping[`export/${function_name}Factory.mts`] = [`template/auto/${function_name}Factory.mts`, "async"]
+	mapping[`export/${function_name}SyncFactory.mts`] = [`template/auto/${function_name}Factory.mts`, "sync"]
+
+	mapping[`export/${function_name}.mts`] = [`template/auto/${function_name}.mts`, "async"]
+	mapping[`export/${function_name}Sync.mts`] = [`template/auto/${function_name}.mts`, "sync"]
+
+	mapping[`export/_DependenciesType.d.mts`] = [`template/auto/DependenciesType.d.mts`, "async"]
+	mapping[`export/_DependenciesSyncType.d.mts`] = [`template/auto/DependenciesType.d.mts`, "sync"]
+
+	mapping[`export/_implementation.mts`] = [`template/implementation.mts`, "async"]
+	mapping[`export/_implementationSync.mts`] = [`template/implementation.mts`, "sync"]
+
+	for (const file_name in mapping) {
+		fourtune_session.autogenerate.addFile(
+			file_name, {
+				generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
+					mapping[file_name][0], mapping[file_name][1]
+				),
+				generator_args: []
+			}
+		)
+	}
 }
-
-/*
-	const {function_name} = fourtune_session.getProjectConfig().target
-
-	fourtune_session.autogenerate.addFile(
-		`export/${function_name}Factory.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/auto/${function_name}Factory.mts`, "async"
-			),
-			generator_args: []
-		}
-	)
-
-	fourtune_session.autogenerate.addFile(
-		`export/${function_name}SyncFactory.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/auto/${function_name}Factory.mts`, "sync"
-			),
-			generator_args: []
-		}
-	)
-
-	fourtune_session.autogenerate.addFile(
-		`export/${function_name}.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/auto/${function_name}.mts`, "async"
-			),
-			generator_args: []
-		}
-	)
-
-	fourtune_session.autogenerate.addFile(
-		`export/${function_name}Sync.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/auto/${function_name}.mts`, "sync"
-			),
-			generator_args: []
-		}
-	)
-
-	fourtune_session.autogenerate.addFile(
-		`export/_DependenciesType.d.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/auto/DependenciesType.d.mts`, "async"
-			),
-			generator_args: []
-		}
-	)
-
-	fourtune_session.autogenerate.addFile(
-		`export/_DependenciesTypeSync.d.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/auto/DependenciesType.d.mts`, "sync"
-			),
-			generator_args: []
-		}
-	)
-
-
-	fourtune_session.autogenerate.addFile(
-		`export/_implementation.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/implementation.mts`, "async"
-			),
-			generator_args: []
-		}
-	)
-
-	fourtune_session.autogenerate.addFile(
-		`export/_implementationSync.mts`, {
-			generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-				`template/implementation.mts`, "sync"
-			),
-			generator_args: []
-		}
-	)
-	*/
