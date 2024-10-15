02439cab03b3a151a2c148ae0bf3accb79ef9de2
eXory2024 <exory@anio.software>
2024-10-15 14:10:13 +0200

    Use invokeTypeScript.

--- src/realm-js/integration/fn/getTypeScriptDefinitions.mjs
+++ src/realm-js/integration/fn/getTypeScriptDefinitions.mjs
@@ -1,20 +1,11 @@
 import {loadRealmDependencies} from "../../auto/base-realm.mjs"
 import getTypeScriptCompilerOptions from "./getTypeScriptCompilerOptions.mjs"
 import path from "node:path"
+import invokeTypeScript from "./invokeTypeScript.mjs"
 
 export default async function(fourtune_session) {
-	const project_root = fourtune_session.getProjectRoot()
-
-	const input_files = fourtune_session.getProjectSourceFiles().filter(({relative_path}) => {
-		if (relative_path.endsWith(".d.mts")) return false
-
-		return relative_path.endsWith(".mts")
-	}).map(({relative_path}) => {
-		return path.join("build", "src", relative_path)
-	})
-
 	const {getDependency} = await loadRealmDependencies(
-		project_root, "realm-js"
+		fourtune_session.getProjectRoot(), "realm-js"
 	)
 
 	const ts = getDependency("typescript")
@@ -24,8 +15,21 @@ export default async function(fourtune_session) {
 		allowJs: false,
 		declaration: true,
 		emitDeclarationOnly: true,
-		noEmitOnError: true
+		noEmitOnError: true,
+		paths: {
+			"#/*": ["./build/src/*"]
+		}
 	}
+
+	const project_root = fourtune_session.getProjectRoot()
+
+	const input_files = fourtune_session.getProjectSourceFiles().filter(({relative_path}) => {
+		if (relative_path.endsWith(".d.mts")) return false
+
+		return relative_path.endsWith(".mts")
+	}).map(({relative_path}) => {
+		return path.join("build", "src", relative_path)
+	})
 
 	//
 	// write files into memory, not on disk
@@ -42,13 +46,17 @@ export default async function(fourtune_session) {
 		memfs.set(file_path, contents)
 	}
 
-	const program = ts.createProgram(input_files, compiler_options, host)
+	const {emitSkipped, diagnostic_messages}  = await invokeTypeScript(
+		ts, input_files, compiler_options, host
+	)
 
-	const result = program.emit()
+	if (emitSkipped) {
+		for (const {message} of diagnostic_messages) {
+			fourtune_session.emitError(`tsc`, message)
+		}
 
-	console.log("ts has errors?", result.emitSkipped)
-
-	if (result.emitSkipped) return
+		return null
+	}
 
 	return memfs
 }
