bbf1a8bc8e4f39ac0b265e0143245d57dc3545aa
eXory2024 <exory@anio.software>
2024-10-15 13:15:33 +0200

    Don't rely on files inside template/auto but rather provide them in memory.

--- src/realm-js/integration/type/async-sync/init.mjs
+++ src/realm-js/integration/type/async-sync/init.mjs
@@ -1,30 +1,55 @@
-export default async function(fourtune_session) {
-	const project_config = fourtune_session.getProjectConfig()
-	const {target} = project_config
-	const {function_name} = target
+import getFilesToAutogenerate from "./getFilesToAutogenerate.mjs"
 
+export default async function(fourtune_session) {
 	const mapping = {}
 
-	mapping[`export/${function_name}Factory.mts`] = [`template/auto/${function_name}Factory.mts`, "async"]
-	mapping[`export/${function_name}SyncFactory.mts`] = [`template/auto/${function_name}Factory.mts`, "sync"]
+	const files = await getFilesToAutogenerate(fourtune_session)
 
-	mapping[`export/${function_name}.mts`] = [`template/auto/${function_name}.mts`, "async"]
-	mapping[`export/${function_name}Sync.mts`] = [`template/auto/${function_name}.mts`, "sync"]
+	for (const file in files) {
+		mapping[file.split("<X>").join("")] = [
+			"generateSyncAsyncVariantFromString",
+			files[file],
+			"async"
+		]
 
-	mapping[`DependenciesType.d.mts`] = [`template/auto/DependenciesType.d.mts`, "async"]
-	mapping[`DependenciesSyncType.d.mts`] = [`template/auto/DependenciesType.d.mts`, "sync"]
+		mapping[file.split("<X>").join("Sync")] = [
+			"generateSyncAsyncVariantFromString",
+			files[file],
+			"sync"
+		]
+	}
 
-	mapping[`ImplementationDocType.d.mts`] = [`template/ImplementationDocType.d.mts`, "async"]
-	mapping[`ImplementationSyncDocType.d.mts`] = [`template/ImplementationDocType.d.mts`, "sync"]
+	mapping[`ImplementationDocType.d.mts`] = [
+		"generateSyncAsyncVariant",
+		`template/ImplementationDocType.d.mts`,
+		"async"
+	]
 
-	mapping[`implementation.mts`] = [`template/implementation.mts`, "async"]
-	mapping[`implementationSync.mts`] = [`template/implementation.mts`, "sync"]
+	mapping[`ImplementationSyncDocType.d.mts`] = [
+		"generateSyncAsyncVariant",
+		`template/ImplementationDocType.d.mts`,
+		"sync"
+	]
+
+	mapping[`implementation.mts`] = [
+		"generateSyncAsyncVariant",
+		`template/implementation.mts`,
+		"async"
+	]
+
+	mapping[`implementationSync.mts`] = [
+		"generateSyncAsyncVariant",
+		`template/implementation.mts`,
+		"sync"
+	]
 
 	for (const file_name in mapping) {
+		const [generator_fn, source, variant] = mapping[file_name]
+
 		fourtune_session.autogenerate.addFile(
 			file_name, {
-				generator:fourtune_session.autogenerate.generateSyncAsyncVariant(
-					mapping[file_name][0], mapping[file_name][1]
+				generator:fourtune_session.autogenerate[generator_fn](
+					source, variant
 				),
 				generator_args: []
 			}
