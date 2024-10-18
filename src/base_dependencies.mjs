297a70e63a061b4e0a89b77b6eae4c911a6114e5
eXory2024 <exory@anio.software>
2024-10-18 15:07:36 +0200

    Fix broken import code.

--- src/base_dependencies.mjs
+++ src/base_dependencies.mjs
@@ -1,6 +1,6 @@
 export default {
 	"@fourtune/realm-js-and-web-utilities": {
 		version: "0.0.4",
-		import_code: `import * as utilities\nexport default utilities from "@fourtune/realm-js-and-web-utilities"`
+		import_code: `import * as utilities from "@fourtune/realm-js-and-web-utilities"\nexport default utilities`
 	}
 }
