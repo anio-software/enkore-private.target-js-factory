b7d023e0e0ed677fa6ddfc30ca7093b2f6ec15ed
eXory2024 <exory@anio.software>
2024-10-18 15:03:20 +0200

    Use @fourtune/realm-js-and-web-utilities.

--- src/base_dependencies.mjs
+++ src/base_dependencies.mjs
@@ -1,34 +1,6 @@
-function useFromJSBundler(import_name) {
-	return {
-		import_from: "@fourtune/js-bundler",
-		import_code: `import {${import_name}} from "@fourtune/js-bundler/exports"\nexport default ${import_name}`
-	}
-}
-
 export default {
-	"@fourtune/js-bundler": {
-		version: "0.0.3"
-	},
-	//
-	// needed to check javascript files
-	//
-	"typescript": useFromJSBundler("ts"),
-	//
-	// used to transpile js code
-	//
-	"@babel/core": {
-		version: "7.25.7"
-	},
-	//
-	// used to strip typescript types
-	//
-	"@babel/preset-typescript": {
-		version: "7.25.7"
-	},
-	//
-	// used to resolve "#"
-	//
-	"babel-plugin-module-resolver": {
-		version: "5.0.2"
+	"@fourtune/realm-js-and-web-utilities": {
+		version: "0.0.3",
+		import_code: `import * as utilities\nexport default utilities from "@fourtune/realm-js-and-web-utilities"`
 	}
 }
