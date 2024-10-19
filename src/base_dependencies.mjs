16600bd21eab0c1edbb6db440c1cfe3ebc67e057
eXory2024 <exory@anio.software>
2024-10-19 22:24:58 +0200

    Bump version of @fourtune/realm-js-and-web-utilities.

--- src/base_dependencies.mjs
+++ src/base_dependencies.mjs
@@ -1,6 +1,6 @@
 export default {
 	"@fourtune/realm-js-and-web-utilities": {
-		version: "0.0.4",
+		version: "0.0.10",
 		import_code: `import * as utilities from "@fourtune/realm-js-and-web-utilities"\nexport default utilities`
 	}
 }
