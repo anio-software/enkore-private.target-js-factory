6dc2ce1744f3921e2d027a3389e17034ca5f9a37
eXory2024 <exory@anio.software>
2024-10-21 12:09:26 +0200

    Re-implement src/realm-js/integration/checkSourceFiles.mjs.

--- src/realm-js/integration/checkSourceFiles.mjs
+++ src/realm-js/integration/checkSourceFiles.mjs
@@ -1,34 +1,41 @@
-/*import path from "node:path"
+import path from "node:path"
 import {loadRealmDependencies} from "../auto/base-realm.mjs"
-import getTypeScriptCompilerOptions from "./fn/getTypeScriptCompilerOptions.mjs"
-import invokeTypeScript from "./fn/invokeTypeScript.mjs"
-*/
-export default async function(fourtune_session) {
-	/*
-	const input_files = fourtune_session.getProjectSourceFiles().filter(({relative_path}) => {
-		return relative_path.endsWith(".mts")
-	}).map(({relative_path}) => {
-		return path.join("build", "src", relative_path)
-	})
 
+export default async function(fourtune_session) {
 	const project_root = fourtune_session.getProjectRoot()
 
 	const {getDependency} = await loadRealmDependencies(
 		project_root, "realm-js"
 	)
 
-	const ts = getDependency("typescript")
+	const {
+		ts,
+		tsReadTSConfigFile,
+		tsInvokeTypeScript
+	} = getDependency(
+		"@fourtune/realm-js-and-web-utilities"
+	)
+
+	const input_files = fourtune_session.getProjectSourceFiles().filter(({relative_path}) => {
+		return relative_path.endsWith(".mts")
+	}).map(({relative_path}) => {
+		return path.join("build", "src", relative_path)
+	})
 
 	const compiler_options = {
-		...await getTypeScriptCompilerOptions(fourtune_session),
+		...await tsReadTSConfigFile(
+			path.join(project_root, "tsconfig.json"), project_root
+		),
 		allowJs: true,
 		checkJs: true,
 		noEmit: true
 	}
 
-	const {diagnostic_messages} = await invokeTypeScript(ts, input_files, compiler_options)
+	const {diagnostic_messages} = await tsInvokeTypeScript(
+		null, input_files, compiler_options
+	)
 
 	for (const {code, message} of diagnostic_messages) {
 		fourtune_session.addWarning(`ts-${code}`, message)
-	}*/
+	}
 }
