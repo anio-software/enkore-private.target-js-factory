9f8b41fc2ebeaf9a391b4aa761dfc093521d2303
eXory2024 <exory@anio.software>
2024-10-21 22:07:38 +0200

    This will break existing usage of @fourtune/realm-js, but only until the new runtime package is ready (the runtime package will not depend on the old system, so this is fine).

---
+++ src/realm-js/integration/_main.mjs
@@ -0,0 +1,39 @@
+import {loadRealmDependencies} from "../auto/base-realm.mjs"
+import path from "node:path"
+
+export function provideBundledSource(
+	options
+) {
+	const {name} = options
+
+	return [{
+		name: `${name}.mjs`,
+		async generator(fourtune_session) {
+			const project_root = fourtune_session.getProjectRoot()
+
+			const {getDependency} = await loadRealmDependencies(
+				project_root, "realm-js"
+			)
+
+			const {tsBundler} = getDependency("@fourtune/base-realm-js-and-web")
+
+			const input = path.join(project_root, options.input)
+
+			return `export default ${JSON.stringify(
+				await tsBundler(
+					project_root,
+					`export * from ${JSON.stringify(input)}\n` +
+					`export {default} from ${JSON.stringify(input)}`,
+					{
+						treeshake: false
+					}
+				)
+			)}`
+		}
+	}, {
+		name: `${name}.d.mts`,
+		generator() {
+			return `declare const _default : string\nexport default _default`
+		}
+	}]
+}
