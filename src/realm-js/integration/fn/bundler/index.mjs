2be6dcb1b3e73bab8f8dd20d40f23aac43c525df
eXory2024 <exory@anio.software>
2024-10-23 23:46:22 +0200

    Use plugin.

--- src/realm-js/integration/fn/bundler/index.mjs
+++ src/realm-js/integration/fn/bundler/index.mjs
@@ -1,5 +1,5 @@
 import {loadRealmDependencies} from "fourtune/base-realm"
-//import fourtuneRollupPlugin from "../../../auto/plugin.mjs"
+import fourtuneRollupPlugin from "../../../auto/plugin.mjs"
 
 export default async function(fourtune_session, options) {
 	const project_root = fourtune_session.getProjectRoot()
@@ -9,18 +9,7 @@ export default async function(fourtune_session, options) {
 
 	const {jsBundler} = getDependency("@fourtune/base-realm-js-and-web")
 
-	return await jsBundler(
-		project_root, options.entry, {
-			input_file_type: options.entry_file_type === "d.mts" ? "dts" : "mjs",
-			minify: options.minified === true,
-			treeshake: true,
-			on_rollup_log_fn(level, {message}) {
-				fourtune_session.addWarning("rollup", {message: `[${level}] rollup says ${message}`})
-			}
-		}
-	)
-
-	/*const additional_rollup_plugins = []
+	const additional_rollup_plugins = []
 
 	if (options.entry_file_type === "mjs") {
 		additional_rollup_plugins.push({
@@ -31,12 +20,14 @@ export default async function(fourtune_session, options) {
 
 	return await jsBundler(
 		project_root, options.entry, {
-			file_type: ,
+			input_file_type: options.entry_file_type === "d.mts" ? "dts" : "mjs",
 			minify: options.minified === true,
-			additional_rollup_plugins,
+			treeshake: true,
+			additional_plugins: additional_rollup_plugins,
 			on_rollup_log_fn(level, {message}) {
-
+				fourtune_session.addWarning("rollup", {message: `[${level}] rollup says ${message}`})
 			}
 		}
-	)*/
+	)
+
 }
