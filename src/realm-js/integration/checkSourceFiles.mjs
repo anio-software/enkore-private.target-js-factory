4b57ec24d8d12e5c5a92fa7858c6f29e1a49d71c
eXory2024 <exory@anio.software>
2024-10-24 16:47:31 +0200

    Also type check source files inside resources/tsmodule.

--- src/realm-js/integration/checkSourceFiles.mjs
+++ src/realm-js/integration/checkSourceFiles.mjs
@@ -1,5 +1,6 @@
 import path from "node:path"
 import {loadRealmDependencies} from "fourtune/base-realm"
+import {scandir} from "@anio-software/fs"
 
 export default async function(fourtune_session) {
 	const project_root = fourtune_session.getProjectRoot()
@@ -16,11 +17,25 @@ export default async function(fourtune_session) {
 		"@fourtune/base-realm-js-and-web"
 	)
 
-	const input_files = fourtune_session.getProjectSourceFiles().filter(({relative_path}) => {
+	const project_source_files = fourtune_session.getProjectSourceFiles().filter(({relative_path}) => {
 		return relative_path.endsWith(".mts")
 	}).map(({relative_path}) => {
 		return path.join("build", "src", relative_path)
 	})
+
+	const project_ts_resources = await scandir(
+		"./resources/tsmodule", {
+			allow_missing_dir: true,
+			map({relative_path}) {
+				return path.join("resources", "tsmodule", relative_path)
+			}
+		}
+	)
+
+	const input_files = [
+		...project_source_files,
+		...project_ts_resources
+	]
 
 	const compiler_options = {
 		...await tsReadTSConfigFile(
