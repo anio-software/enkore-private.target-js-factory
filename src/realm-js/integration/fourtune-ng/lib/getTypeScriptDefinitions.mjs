794f97ee0e3b97b502c52c70cdbc16dde4847569
eXory2024 <exory@anio.software>
2024-10-29 20:03:05 +0100

    (wip) integration for next generation of fourtune.

---
+++ src/realm-js/integration/fourtune-ng/lib/getTypeScriptDefinitions.mjs
@@ -0,0 +1,61 @@
+import path from "node:path"
+
+export async function getTypeScriptDefinitions(fourtune_session, input_files) {
+	const project_root = fourtune_session.getProjectRoot()
+
+	const {
+		ts,
+		tsReadTSConfigFile,
+		tsInvokeTypeScript
+	} = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
+
+	// todo: handle tsconfig.src.json/tsconfig.resources.json correctly
+	const compiler_options = {
+		...await tsReadTSConfigFile(
+			path.join(project_root, "auto", "cfg", "tsconfig.base.json")
+		),
+		allowJs: false,
+		declaration: true,
+		emitDeclarationOnly: true,
+		noEmitOnError: true,
+		baseUrl: "./"
+	}
+
+	compiler_options.paths = {
+		...compiler_options.paths,
+		// overwrite # alias to point to "build/src" instead of "src/"
+		"#/*":  ["./.fourtune/v0/build/src/*"],
+		"##/*": ["./.fourtune/v0/build/auto/src/*"],
+		"&/*":  ["./.fourtune/v0/build/assets/tsmodule/*"],
+		"&&/*": ["./.fourtune/v0/build/auto/assets/tsmodule/*"]
+	}
+
+	//
+	// write files into memory, not on disk
+	//
+	const host = ts.createCompilerHost(compiler_options)
+
+	let memfs = new Map()
+
+	host.writeFile = (file_path, contents) => {
+		if (file_path.startsWith(project_root + "/")) {
+			file_path = file_path.slice(project_root.length + 1)
+		}
+
+		memfs.set(file_path, contents)
+	}
+
+	const {errors, diagnostic_messages}  = await tsInvokeTypeScript(
+		host, input_files, compiler_options
+	)
+
+	if (errors) {
+		for (const {message} of diagnostic_messages) {
+			fourtune_session.emitError(`tsc`, message)
+		}
+
+		return null
+	}
+
+	return memfs
+}
---
+++ src/realm-js/integration/fourtune-ng/lib/init/initPackageLikeProject.mjs
@@ -0,0 +1,133 @@
+function getExportTypeAndName(filename) {
+	if (filename.endsWith(".d.mts")) {
+		return {
+			type: "d.mts",
+			name: filename.slice(0, -6)
+		}
+	} else if (filename.endsWith(".mts")) {
+		return {
+			type: "mts",
+			name: filename.slice(0, -4)
+		}
+	}
+
+	return false
+}
+
+function importStatement(source, export_name, is_type = false) {
+	const source_str = JSON.stringify(source)
+	const t_export = is_type ? " type" : ""
+
+	//
+	// treat __star_export differently so such
+	// that this export may manually export
+	// other things.
+	//
+	if (export_name === "__star_export") {
+		return `export${t_export} * from ${source_str}`
+	} else if (export_name === "__default") {
+		return `export${t_export} {default} from ${source_str}`
+	} else if (export_name === "__index") {
+		return `export${t_export} * from ${source_str}\n` +
+		       `export${t_export} {default} from ${source_str}`
+	} else {
+		//
+		// Normally, the file name is used to
+		// create a named export in the output module.
+		// This means, myFunction.mjs would be exported as
+		// "myFunction"
+		//
+		return `export${t_export} {${export_name}} from ${source_str}`
+	}
+}
+
+export async function initPackageLikeProject(fourtune_session) {
+	const output_modules = new Map()
+
+	for (const source of fourtune_session.input.getSourceFiles()) {
+		if (!source.parents.length) continue
+		if (source.parents[0] !== "export") continue
+
+		const parsed = getExportTypeAndName(source.name)
+
+		if (!parsed) continue
+
+		source.parents = source.parents.slice(1)
+
+		const export_name = parsed.name
+		const module_name = source.parents.length ? source.parents.join(".") : "default"
+
+		if (!output_modules.has(module_name)) {
+			output_modules.set(module_name, new Map())
+		}
+
+		const module_exports = output_modules.get(module_name)
+
+		if (module_exports.has(export_name)) {
+			const using = module_exports.get(export_name)
+
+			fourtune_session.emitWarning(
+				`pkg.duplicate_export`, {
+					module_name, export_name, using
+				}
+			)
+		} else {
+			module_exports.set(export_name, source.source)
+		}
+	}
+
+	for (const [module_name, module_exports] of output_modules.entries()) {
+		const product = fourtune_session.products.addProduct(module_name)
+
+		product.addDistributable(
+			"bundle", "index.mjs", async () => {
+				const {tsBundler} = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
+				let entry_code = ``
+
+				for (const [export_name, source] of module_exports.entries()) {
+					if (source.endsWith(".d.mts") || !source.endsWith(".mts")) {
+						continue
+					}
+
+					const extensionless_source = source.slice(0, -4)
+
+					entry_code += importStatement(
+						"./.fourtune/v0/objects/" + extensionless_source + ".mjs", export_name, false
+					)
+				}
+
+				return await tsBundler(
+					fourtune_session.getProjectRoot(),
+					entry_code, {}
+				)
+			}
+		)
+
+		product.addDistributable("types", "index.d.mts",
+			async () => {
+				const {jsBundler} = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
+				let entry_code = ``
+
+				for (const [export_name, source] of module_exports.entries()) {
+					if (source.endsWith(".d.mts")) {
+
+					} else if (source.endsWith(".mts")) {
+						const extensionless_source = source.slice(0, -4)
+
+						entry_code += importStatement(
+							"./.fourtune/v0/objects/" + extensionless_source + ".d.mts", export_name, true
+						)
+					}
+				}
+
+				return await jsBundler(
+					fourtune_session.getProjectRoot(),
+					entry_code, {
+						input_file_type: "dts",
+						on_rollup_log_fn: console.log
+					}
+				)
+			}
+		)
+	}
+}
