7f56c7b6ec62c63d21b99dc5eac7d9279501a1f2
eXory2024 <exory@anio.software>
2024-10-30 23:48:27 +0100

    Rename file and use named export.

--- src/integration/lib/init/async-sync/initializeAsyncSyncProject.mjs
+++ src/integration/lib/init/async-sync/initializeAsyncSyncProject.mjs
@@ -1,4 +1,4 @@
-export default async function(fourtune_session, writeFile) {
+export async function initializeAsyncSyncProject(fourtune_session, writeFile) {
 	await writeFile(
 		`src/template/implementation.mts`,
 
