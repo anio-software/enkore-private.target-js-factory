108eef9100e6728e47d4bb34d958ac9689cc85cb
eXory2024 <exory@anio.software>
2024-11-05 22:58:10 +0100

    Replace fourtune/base-realm with @fourtune/core.

--- src/_install.mjs
+++ src/_install.mjs
@@ -1,29 +1,5 @@
-import path from "node:path"
 import process from "node:process"
 import dependencies from "./dependencies.mjs"
-
-async function runInstall() {
-	const {
-		findProjectRootFromDirectory,
-		installRealmDependencies
-	} = await import("fourtune/base-realm")
-
-	const start_dir = path.dirname(process.argv[1])
-
-	const project_root = await findProjectRootFromDirectory(
-		start_dir
-	)
-
-	if (project_root === false) {
-		throw new Error(
-			`Unable to determine project root. Start directory is '${start_dir}'.`
-		)
-	}
-
-	await installRealmDependencies(project_root, "realm-js", {
-		...dependencies
-	})
-}
 
 // skip installation of realm dependencies in CI/CD environment
 // because this package doesn't have the "fourtune" package as a
@@ -35,5 +11,9 @@ if ("ANIO_CICD_REPO" in process.env) {
 }
 
 if (!skip_install) {
-	await runInstall()
+	const {installRealmDependencies} = await import("@fourtune/core")
+
+	await installRealmDependencies(
+		"cli", "js", dependencies
+	)
 }
