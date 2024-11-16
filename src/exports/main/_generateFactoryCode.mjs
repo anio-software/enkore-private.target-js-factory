ddaf934740ba1ebd5e741be0b62b1f45fce1d618
eXory2024 <exory@anio.software>
2024-11-16 02:43:35 +0100

    Make autogenerated code look more pretty.

--- src/exports/main/_generateFactoryCode.mjs
+++ src/exports/main/_generateFactoryCode.mjs
@@ -41,22 +41,26 @@ export function _generateFactoryCode(
 		dependencies_init = dependencies_init.slice(0, -2)
 	}
 
+	if (dependencies_import.length) {
+		dependencies_import = `\n${dependencies_import}`
+	}
+
+	if (dependencies_init.length) {
+		dependencies_init = `\n${dependencies_init}\n\t`
+	}
+
 	return `
 import {${import_name} as implementation} from ${src}
 import type {Dependencies, Signature} from ${src}
 import type {UserContext} from "@fourtune/realm-js/v0/runtime"
 import {getProject} from "@fourtune/realm-js/v0/project"
 import {useContext} from "@fourtune/realm-js/v0/runtime"
-
 ${dependencies_import}
-
 export function ${export_name}(user: UserContext = {}) : Signature {
 	const project = getProject()
 	const context = useContext(project, user)
 
-	const dependencies : Dependencies = {
-${dependencies_init}
-	}
+	const dependencies : Dependencies = {${dependencies_init}}
 
 	return ${is_async ? "async " : ""}function(...args: Parameters<Signature>) : ReturnType<Signature> {
 		return ${is_async ? "await ": ""}implementation(context, dependencies, ...args)
