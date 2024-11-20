82c89ea0bf8c5434d463567bf8ed7ac68db8ae49
eXory2024 <exory@anio.software>
2024-11-20 05:19:06 +0100

    async/sync create function files.

--- src/exports/main/generateFactoryFiles/_generateAsyncSyncFactoryFiles.mjs
+++ src/exports/main/generateFactoryFiles/_generateAsyncSyncFactoryFiles.mjs
@@ -1,4 +1,7 @@
 import {getPaths} from "./getPaths.mjs"
+import {_generateFunctionCode} from "./_generateFunctionCode.mjs"
+import {expandAsyncSyncVariantNew} from "../../../expandAsyncSyncVariantName.mjs"
+import path from "node:path"
 
 export function _generateAsyncSyncFactoryFiles(
 	options
@@ -6,9 +9,21 @@ export function _generateAsyncSyncFactoryFiles(
 	let ret = {}
 
 	const [async_paths, sync_paths] = getPaths(options)
+	const [async_export_name, sync_export_name] = expandAsyncSyncVariantNew(
+		options.export_name
+	)
 
-	ret[async_paths.output.fn] = () => ""
-	ret[sync_paths.output.fn] = () => ""
+	ret[async_paths.output.fn] = () => _generateFunctionCode(
+		path.join("#~auto", async_paths.output.factory),
+		async_paths.source,
+		async_export_name
+	)
+
+	ret[sync_paths.output.fn] = () => _generateFunctionCode(
+		path.join("#~auto", sync_paths.output.factory),
+		sync_paths.source,
+		sync_export_name
+	)
 
 	ret[async_paths.output.factory] = () => ""
 	ret[sync_paths.output.factory] = () => ""
