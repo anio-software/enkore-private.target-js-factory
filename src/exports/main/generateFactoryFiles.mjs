2537dfee9c5d224f6c235f58d99b6faf637f0d6a
eXory2024 <exory@anio.software>
2024-11-20 04:57:55 +0100

    Remove implementation for now.

--- src/exports/main/generateFactoryFiles.mjs
+++ src/exports/main/generateFactoryFiles.mjs
@@ -1,8 +1,5 @@
-import {expandAsyncSyncVariantFilePath} from "../../expandAsyncSyncVariantName.mjs"
-import {_generateFactoryCode} from "./_generateFactoryCode.mjs"
-import {_generateFunctionCode} from "./_generateFunctionCode.mjs"
-import fs from "node:fs/promises"
-import path from "node:path"
+import {isExpandableFileName, isExpandableName} from "../../expandAsyncSyncVariantName.mjs"
+import {_generateAsyncSyncFactoryFiles} from "./_generateAsyncSyncFactoryFiles.mjs"
 
 /*
 input object:
@@ -14,8 +11,6 @@ input object:
 export function generateFactoryFiles(
 	options
 ) {
-	const is_async_sync_source = options.source_file.endsWith(".as.mts")
-
 	const required_options = ["source_file", "export_name", "destination"]
 
 	for (const o of required_options) {
@@ -24,120 +19,19 @@ export function generateFactoryFiles(
 		}
 	}
 
-	let ret = {}
-
-	if (!options.source_file.startsWith("src/")) {
-		throw new Error(`source file must be inside src/.`)
-	} else if (!options.destination.startsWith("src/")) {
-		throw new Error(`destination must start with src/.`)
-	}
-
-	const base = `${options.destination}/${options.export_name}`
-	const base_without_src = `${options.destination.slice(4)}/${options.export_name}`
-
-	if (is_async_sync_source) {
-		const [async_path, sync_path] = expandAsyncSyncVariantFilePath(options.source_file.slice(4))
-
-		ret[`${base}.mts`] = async function(fourtune_session) {
-			return _generateFunctionCode(
-				`#~auto/${base_without_src}Factory.mts`,
-				`${options.export_name}Factory`,
-				`#~auto/${async_path}`,
-				options.export_name
+	//
+	// if input source file is an async/sync variant file
+	// export_name **must** also be expandable
+	//
+	if (isExpandableFileName(options.source_file)) {
+		if (!isExpandableName(options.export_name)) {
+			throw new Error(
+				`Cannot have async/sync variant source without expandable export_name.`
 			)
 		}
 
-		ret[`${base}Sync.mts`] = async function(fourtune_session) {
-			return _generateFunctionCode(
-				`#~auto/${base_without_src}SyncFactory.mts`,
-				`${options.export_name}SyncFactory`,
-				`#~auto/${sync_path}`,
-				`${options.export_name}Sync`
-			)
-		}
-
-		ret[`${base}Factory.mts`] = async function(fourtune_session) {
-			const {generateAsyncSyncVariant} = fourtune_session.autogenerate
-
-			// NB: we must create our own version of async/sync
-			// since we would be potentially using outdated code
-			// (e.g. if we read the code from the file system (auto/src/ folder))
-			const generate = await generateAsyncSyncVariant(
-				options.source_file, "async"
-			)
-
-			const source = await generate(fourtune_session)
-			const base_object = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
-			const {tsGetDeclaredAnioSoftwareDependenciesFromCode} = base_object
-			const dependencies = await tsGetDeclaredAnioSoftwareDependenciesFromCode(source)
-
-			return _generateFactoryCode(
-				`#~auto/${async_path}`,
-				"implementation",
-				`${options.export_name}Factory`,
-				dependencies,
-				true
-			)
-		}
-
-		ret[`${base}SyncFactory.mts`] = async function(fourtune_session) {
-			const {generateAsyncSyncVariant} = fourtune_session.autogenerate
-
-			// NB: we must create our own version of async/sync
-			// since we would be potentially using outdated code
-			// (e.g. if we read the code from the file system (auto/src/ folder))
-			const generate = await generateAsyncSyncVariant(
-				options.source_file, "sync"
-			)
-
-			const source = await generate(fourtune_session)
-			const base_object = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
-			const {tsGetDeclaredAnioSoftwareDependenciesFromCode} = base_object
-			const dependencies = await tsGetDeclaredAnioSoftwareDependenciesFromCode(source)
-
-			return _generateFactoryCode(
-				`#~auto/${sync_path}`,
-				"implementationSync",
-				`${options.export_name}SyncFactory`,
-				dependencies,
-				false
-			)
-		}
-
-		return ret
+		return _generateAsyncSyncFactoryFiles(options)
 	}
 
-	ret[`${base}.mts`] = async function(fourtune_session) {
-		return _generateFunctionCode(
-			`#~auto/${base_without_src}Factory.mts`,
-			`${options.export_name}Factory`,
-			`#~${options.source_file}`,
-			"implementation"
-		)
-	}
-
-	ret[`${base}Factory.mts`] = async function(fourtune_session) {
-		const source = (await fs.readFile(
-			path.join(fourtune_session.getProjectRoot(), options.source_file)
-		)).toString()
-
-		const base_object = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
-		const {
-			tsGetDeclaredAnioSoftwareDependenciesFromCode,
-			tsGetExportedFunctionFromCode
-		} = base_object
-
-		const dependencies = await tsGetDeclaredAnioSoftwareDependenciesFromCode(source)
-		const {is_async} = await tsGetExportedFunctionFromCode(source, "implementation")
-
-		return _generateFactoryCode(
-			`#~${options.source_file}`,
-			"implementation",
-			`${options.export_name}Factory`,
-			dependencies,
-			is_async
-		)
-	}
-
-	return ret
+	return {}
 }
