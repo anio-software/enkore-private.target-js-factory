f50c040e0d2fda66e6d8946d34c0a91b6525a4b8
eXory2024 <exory@anio.software>
2024-11-20 04:58:54 +0100

    Add _generateAsyncSyncFactoryFiles.

---
+++ src/exports/main/generateFactoryFiles/_generateAsyncSyncFactoryFiles.mjs
@@ -0,0 +1,19 @@
+import {getPaths} from "./getPaths.mjs"
+
+export function _generateAsyncSyncFactoryFiles(
+	options
+) {
+	let ret = {}
+
+	const [async_paths, sync_paths] = getPaths(options)
+
+	console.log(async_paths, sync_paths)
+
+	ret[async_paths.output.fn] = () => ""
+	ret[async_paths.output.factory] = () => ""
+
+	ret[sync_paths.output.fn] = () => ""
+	ret[sync_paths.output.factory] = () => ""
+
+	return ret
+}
