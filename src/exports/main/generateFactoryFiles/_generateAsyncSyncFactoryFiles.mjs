1249a0eb4881ca99fd670e1df694c3d5e76071bc
eXory2024 <exory@anio.software>
2024-11-25 08:30:25 +0100

    Use "tsGenerateFunctionFactoryCode" to generate the function code as well.

--- src/exports/main/generateFactoryFiles/_generateAsyncSyncFactoryFiles.mjs
+++ src/exports/main/generateFactoryFiles/_generateAsyncSyncFactoryFiles.mjs
@@ -3,6 +3,31 @@ import {_generateFunctionCode} from "./_generateFunctionCode.mjs"
 import {expandAsyncSyncVariantNew} from "../../../expandAsyncSyncVariantName.mjs"
 import path from "node:path"
 
+function generateFunctionFileFactory(options, paths, variant) {
+	return async (fourtune_session) => {
+		const {generateAsyncSyncVariant} = fourtune_session.autogenerate
+
+		// NB: we must create our own version of async/sync
+		// since we would be potentially using outdated code
+		// (e.g. if we read the code from the file system (auto/src/ folder))
+		const generate = generateAsyncSyncVariant(options.source_file, variant)
+		const source = await  generate(fourtune_session, variant)
+		const base = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
+
+		const {
+			tsGenerateFunctionFactoryCode
+		} = base
+
+		const {fn} = await tsGenerateFunctionFactoryCode(
+			paths,
+			source,
+			variant === "async"
+		)
+
+		return fn
+	}
+}
+
 function generateFactoryFileFactory(options, paths, variant) {
 	return async (fourtune_session) => {
 		const {generateAsyncSyncVariant} = fourtune_session.autogenerate
@@ -34,20 +59,13 @@ export function _generateAsyncSyncFactoryFiles(
 	let ret = {}
 
 	const [async_paths, sync_paths] = getPaths(options)
-	const [async_export_name, sync_export_name] = expandAsyncSyncVariantNew(
-		options.export_name
+
+	ret[async_paths.output.fn] = generateFunctionFileFactory(
+		options, async_paths, "async"
 	)
 
-	ret[async_paths.output.fn] = () => _generateFunctionCode(
-		path.join("#~auto", async_paths.output.factory),
-		async_paths.source,
-		async_export_name
-	)
-
-	ret[sync_paths.output.fn] = () => _generateFunctionCode(
-		path.join("#~auto", sync_paths.output.factory),
-		sync_paths.source,
-		sync_export_name
+	ret[sync_paths.output.fn] = generateFunctionFileFactory(
+		options, sync_paths, "sync"
 	)
 
 	ret[async_paths.output.factory] = generateFactoryFileFactory(
--- src/exports/main/generateFactoryFiles/_generateFactoryFiles.mjs
+++ src/exports/main/generateFactoryFiles/_generateFactoryFiles.mjs
@@ -9,11 +9,25 @@ export function _generateFactoryFiles(
 	let ret = {}
 	const paths = getPaths(options)
 
-	ret[paths.output.fn] = () => _generateFunctionCode(
-		path.join("#~auto", paths.output.factory),
-		paths.source,
-		options.export_name
-	)
+	ret[paths.output.fn] = async (fourtune_session) => {
+		const source_code = (await fs.readFile(
+			path.join(fourtune_session.getProjectRoot(), options.source_file)
+		)).toString()
+
+		const base = await fourtune_session.getDependency("@fourtune/base-realm-js-and-web")
+
+		const {
+			tsGenerateFunctionFactoryCode
+		} = base
+
+		const {fn} = await tsGenerateFunctionFactoryCode(
+			paths,
+			source_code,
+			null
+		)
+
+		return fn
+	}
 
 	ret[paths.output.factory] = async (fourtune_session) => {
 		const source_code = (await fs.readFile(
