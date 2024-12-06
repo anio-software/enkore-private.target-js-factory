a9bf1d5bc612a8427af44fe71b5736ea5f3d02c5
eXory2024 <exory@anio.software>
2024-12-06 13:40:55 +0100

    Expansion of async/sync files is now provided by the utils API.

--- src/integration/lib/init/_generic/preInitializeGenericProject.mjs
+++ src/integration/lib/init/_generic/preInitializeGenericProject.mjs
@@ -1,10 +1,21 @@
 import path from "node:path"
-import {isExpandableFilePath, expandAsyncSyncVariantFilePath} from "@fourtune/js-and-web-runtime-and-rollup-plugins/v0/utils-api"
-import {generateAsyncSyncVariant} from "@fourtune/js-and-web-runtime-and-rollup-plugins/v0/autogenerate-api"
+import {
+	isExpandableFilePath,
+	expandAsyncSyncVariantFilePath,
+	expandAsyncSyncVariantSourceFile
+} from "@fourtune/js-and-web-runtime-and-rollup-plugins/v0/utils-api"
 
 export async function preInitializeGenericProject(
 	fourtune_session, source_files
 ) {
+	const asyncSyncGeneratorFactory = function(src_file, variant) {
+		return function (fourtune_session) {
+			return expandAsyncSyncVariantSourceFile(
+				path.join(fourtune_session.getProjectRoot(), src_file), variant
+			)
+		}
+	}
+
 	for (const src of source_files) {
 		if (!isExpandableFilePath(src.name)) continue
 
@@ -16,13 +27,13 @@ export async function preInitializeGenericProject(
 		fourtune_session.autogenerate.addUserFile(
 			path.join(
 				path.dirname(src.source), async_file_name
-			), generateAsyncSyncVariant(src.source, "async")
+			), asyncSyncGeneratorFactory(src.source, "async")
 		)
 
 		fourtune_session.autogenerate.addUserFile(
 			path.join(
 				path.dirname(src.source), sync_file_name
-			), generateAsyncSyncVariant(src.source, "sync")
+			), asyncSyncGeneratorFactory(src.source, "sync")
 		)
 	}
 }
