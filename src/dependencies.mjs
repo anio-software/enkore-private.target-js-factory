57e1bbb209078e851440cfffe3d7cdb523cc88d9
eXory2024 <exory@anio.software>
2024-12-09 22:18:58 +0100

    Bump dependencies.

--- src/dependencies.mjs
+++ src/dependencies.mjs
@@ -5,11 +5,11 @@ export default {
 	api_version: 1,
 	dependencies: {
 		"@fourtune/base-realm-js-and-web": {
-			version: "0.30000.2"
+			version: "0.30000.3"
 		},
 
 		"@aniojs/node-ts-utils": {
-			version: "0.2.0",
+			version: "0.3.0",
 			import_kind: "star"
 		}
 	}
