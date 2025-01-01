06009300d698e7e603bbb34b373cf5442c24f4da
eXory2024 <exory@anio.software>
2025-01-01 02:48:32 +0100

    Refactor: rename variable.

--- src/integration/lib/init/package-like/getEntryCode.mjs
+++ src/integration/lib/init/package-like/getEntryCode.mjs
@@ -1,11 +1,11 @@
 import {exportStatement} from "./exportStatement.mjs"
 
-export function getEntryCode(fourtune_session, module_exports) {
+export function getEntryCode(fourtune_session, entryPointExportMap) {
 	const {getObjectsPath} = fourtune_session.paths
 
 	let entry_code = ``
 
-	for (const [export_name, {type, extensionlessSource}] of module_exports.entries()) {
+	for (const [export_name, {type, extensionlessSource}] of entryPointExportMap.entries()) {
 		if (type !== "mts") continue
 
 		entry_code += exportStatement(
