b67c4bd13bff5f8e7a53763182857db65a0acbbf
eXory2024 <exory@anio.software>
2025-01-01 02:18:34 +0100

    Refactor code.

--- src/integration/lib/init/package-like/getEntryCode.mjs
+++ src/integration/lib/init/package-like/getEntryCode.mjs
@@ -5,14 +5,11 @@ export function getEntryCode(fourtune_session, module_exports) {
 
 	let entry_code = ``
 
-	for (const [export_name, {source}] of module_exports.entries()) {
-		if (source.endsWith(".d.mts")) continue
-		if (!source.endsWith(".mts")) continue
-
-		const extensionless_source = source.slice(0, -4)
+	for (const [export_name, {type, extensionlessSource}] of module_exports.entries()) {
+		if (type !== "mts") continue
 
 		entry_code += exportStatement(
-			getObjectsPath(`${extensionless_source + ".mjs"}`),
+			getObjectsPath(`${extensionlessSource}.mjs`),
 			export_name, false
 		)
 	}
