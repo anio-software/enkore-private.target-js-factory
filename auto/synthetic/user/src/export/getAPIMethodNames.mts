2c5c67a2d6f26aa1ad02f15e8464f840867fdd91
eXory2024 <exory@anio.software>
2025-04-01 20:52:12 +0200

    Add getAPIMethodNames which returns an array of all API methods.

---
+++ auto/synthetic/user/src/export/getAPIMethodNames.mts
@@ -0,0 +1,3 @@
+export function getAPIMethodNames() {
+	return ["compile","generateProduct","getBoilerplateFiles","getGitIgnoredFiles","getInitialInternalData","getRealmDependenciesToInstall","preCompile","preInitialize","preLint","initialize","lint","preprocess","projectSourceFileFilter","publishProduct","testProduct"]
+}
--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,6 +1,38 @@
+import {scandir} from "@aniojs/node-fs"
+import path from "node:path"
+import {fileURLToPath} from "node:url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const apiFactoryFiles = await scandir(
+	path.join(__dirname, "src", "public"),
+	{
+		sorted: true,
+		filter(e) {
+			if (e.type !== "regularFile") return false
+			if (!e.name.includes("Factory")) return false
+			if (!e.name.endsWith(".mts")) return false
+
+			return true
+		}
+	}
+)
+
+const apiMethods = apiFactoryFiles.map(e => {
+	return e.name.slice(0, -("Factory.mts".length))
+})
+
 export default {
 	realm: {
 		name: "js",
 		type: "package"
+	},
+
+	autogenerate: {
+		"src/export/getAPIMethodNames.mts": function() {
+			return `export function getAPIMethodNames() {
+	return ${JSON.stringify(apiMethods)}
+}\n`
+		}
 	}
 }
