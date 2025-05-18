2813e1c9da104d2c9302107e21ba4d8783b8c913
eXory2024 <exory@anio.software>
2025-05-19 00:12:16 +0200

    Also publish in @anio-software namespace.

--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -4,6 +4,8 @@ if [[ "$RELEASE_VERSION" == vp* ]]; then
 	npm publish --provenance --access public
 else
 	node ./.cicd/updatePackageName.mjs "@asint/enkore__target-js-factory"
+	npm publish --access public
 
+	node ./.cicd/updatePackageName.mjs "@anio-software/enkore.target-js-factory"
 	npm publish --access public
 fi
