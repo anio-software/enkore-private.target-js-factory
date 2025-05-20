86da6d32ae4861e5354c3bd70182168f06b63d88
eXory2024 <exory@anio.software>
2025-05-20 03:33:27 +0200

    Don't change package name.

--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -3,6 +3,5 @@
 if [[ "$RELEASE_VERSION" == vp* ]]; then
 	npm publish --provenance --access public
 else
-	node ./.cicd/updatePackageName.mjs "@anio-software/enkore.target-js-factory"
 	npm publish --access public
 fi
