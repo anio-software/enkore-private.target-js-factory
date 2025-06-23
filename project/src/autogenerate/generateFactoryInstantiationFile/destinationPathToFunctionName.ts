8c1c4b416fe7ea135e65930cd6ad7e600e0fb59c
eXory2024 <exory@anio.software>
2025-06-23 11:37:27 +0200

    Update destinationPathToFunctionName.

--- project/src/autogenerate/generateFactoryInstantiationFile/destinationPathToFunctionName.ts
+++ project/src/autogenerate/generateFactoryInstantiationFile/destinationPathToFunctionName.ts
@@ -3,9 +3,9 @@ import path from "node:path"
 export function destinationPathToFunctionName(destinationPath: string): string {
 	const basename = path.basename(destinationPath)
 
-	if (basename.endsWith(".as.mts")) {
-		return basename.slice(0, -(".as.mts".length))
+	if (basename.endsWith(".as.ts")) {
+		return basename.slice(0, -(".as.ts".length))
 	}
 
-	return basename.slice(0, -(".mts".length))
+	return basename.slice(0, -(".ts".length))
 }
