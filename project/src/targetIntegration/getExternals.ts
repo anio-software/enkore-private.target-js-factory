1431eea41977f601abc5aeb66d5bf47b5bd917b9
eXory2024 <exory@anio.software>
2025-07-03 00:57:35 +0200

    User decides whether react packages should be bundled or not (default is not to bundle).

--- project/src/targetIntegration/getExternals.ts
+++ project/src/targetIntegration/getExternals.ts
@@ -30,21 +30,15 @@ export function getExternals(
 		}
 	}
 
-	const bundleReact: boolean = (() => {
-		if (apiContext.target === "jsx-web") {
-			return true
-		} else if (apiContext.target === "jsx-hybrid") {
-			return true
+	if (apiContext.target === "jsx-web" || apiContext.target === "jsx-hybrid") {
+		const {bundleReactPackages} = session.target.getOptions(apiContext.target)
+
+		if (bundleReactPackages !== true) {
+			externals.set(`react`, 1)
+			externals.set(`react/jsx-runtime`, 1)
+			externals.set(`react-dom/client`, 1)
+			externals.set(`react-dom/server`, 1)
 		}
-
-		return false
-	})()
-
-	if (!bundleReact) {
-		externals.set(`react`, 1)
-		externals.set(`react/jsx-runtime`, 1)
-		externals.set(`react-dom/client`, 1)
-		externals.set(`react-dom/server`, 1)
 	}
 
 	return [...externals.entries()].map(([key]) => {
