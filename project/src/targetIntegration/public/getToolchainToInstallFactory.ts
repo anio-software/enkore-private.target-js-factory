36e80227a43d1f778ce70e47a2d225fc72f552ea
eXory2024 <exory@anio.software>
2025-07-02 21:27:09 +0200

    Bump toolchain.

--- project/src/targetIntegration/public/getToolchainToInstallFactory.ts
+++ project/src/targetIntegration/public/getToolchainToInstallFactory.ts
@@ -4,7 +4,7 @@ import type {APIContext} from "#~src/targetIntegration/APIContext.ts"
 const impl: API["getToolchainToInstall"] = async function(
 	this: APIContext, earlySession
 ) {
-	return ["js", 56]
+	return ["js", 57]
 }
 
 export function getToolchainToInstallFactory(context: APIContext) {
