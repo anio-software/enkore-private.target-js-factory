fb9f645a6d3eacc416c91401ab32cb601bfcef15
eXory2024 <exory@anio.software>
2025-07-06 00:55:31 +0200

    Identifier for global data records has changed.

--- project/embeds/project/getGlobalRuntimeDataRecords.ts
+++ project/embeds/project/getGlobalRuntimeDataRecords.ts
@@ -1,10 +1,10 @@
 import type {EnkoreJSRuntimeGlobalDataRecord} from "@anio-software/enkore-private.spec"
 
 export function getGlobalRuntimeDataRecords(): EnkoreJSRuntimeGlobalDataRecord[] {
-	const globalThisPropKey = Symbol.for("@enkore/js-runtime/globalRecords")
+	const globalThisPropKey = Symbol.for("@anio-software/enkore.js-runtime/globalRecords")
 
 	if (!(globalThisPropKey in globalThis)) {
-		throw new Error(`globalThis[Symbol.for("@enkore/js-runtime/globalRecords")] is not set. This is a bug.`)
+		throw new Error(`globalThis[Symbol.for("@anio-software/enkore.js-runtime/globalRecords")] is not set. This is a bug.`)
 	}
 
 	const records = (
