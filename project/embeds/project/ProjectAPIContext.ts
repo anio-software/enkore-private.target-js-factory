181e36bbcbcd3b884569df831314acbda3757e95
eXory2024 <exory@anio.software>
2025-07-06 00:56:05 +0200

    Update package name.

--- project/embeds/project/ProjectAPIContext.ts
+++ project/embeds/project/ProjectAPIContext.ts
@@ -8,7 +8,7 @@ export type ProjectEmbedFile = {
 }
 
 export type ProjectAPIContext = {
-	// pre-computed here so we don't have to import "@enkore/spec"
+	// pre-computed here so we don't have to import "@anio-software/enkore-private.spec"
 	// which leads to inclusion of node-specific modules
 	// (because of disabled treeshaking?)
 	project: EnkoreJSRuntimeProject
