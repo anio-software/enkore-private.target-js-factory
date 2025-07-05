1208573eeb3a14c2080b03fafa081f8148c99675
eXory2024 <exory@anio.software>
2025-07-05 18:47:14 +0200

    getInternalData was renamed to __getInternalData.

--- project/src/targetIntegration/getInternalData.ts
+++ project/src/targetIntegration/getInternalData.ts
@@ -2,5 +2,5 @@ import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec"
 import type {InternalData} from "./InternalData.ts"
 
 export function getInternalData(session: EnkoreSessionAPI) {
-	return session.target.getInternalData() as InternalData
+	return session.target.__getInternalData() as InternalData
 }
