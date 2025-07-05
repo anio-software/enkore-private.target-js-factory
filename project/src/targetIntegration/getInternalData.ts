121eb8a9ebaacff2ac8fbd46e49cb2f8d241e109
eXory2024 <exory@anio.software>
2025-07-05 18:48:21 +0200

    Provide backwards compatibility for old API.

--- project/src/targetIntegration/getInternalData.ts
+++ project/src/targetIntegration/getInternalData.ts
@@ -2,5 +2,12 @@ import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec"
 import type {InternalData} from "./InternalData.ts"
 
 export function getInternalData(session: EnkoreSessionAPI) {
+	// temporary backwards compat
+	if ("getInternalData" in session.target) {
+		session.enkore.emitMessage("warning", `USING OLD getInternalData() API`)
+
+		return (session.target as any).getInternalData() as InternalData
+	}
+
 	return session.target.__getInternalData() as InternalData
 }
