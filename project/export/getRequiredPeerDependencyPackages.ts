ee67c87f6009c57652ade615b927255acf4183ef
eXory2024 <exory@anio.software>
2025-07-05 18:58:19 +0200

    Bump @anio-software/enkore.js-runtime.

--- project/export/getRequiredPeerDependencyPackages.ts
+++ project/export/getRequiredPeerDependencyPackages.ts
@@ -13,7 +13,7 @@ const packageVersionRanges: Record<string, string> = {
 
 	"css-modules-ts-plugin": ">=0.0.8",
 
-	"@anio-software/enkore.js-runtime": ">=0.0.9"
+	"@anio-software/enkore.js-runtime": ">=0.0.10"
 }
 
 export function getRequiredPeerDependencyPackages(
