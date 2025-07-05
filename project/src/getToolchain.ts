3edd7dc134b3f3ad050a71910cf7750481845791
eXory2024 <exory@anio.software>
2025-07-05 18:33:45 +0200

    Provide backwards compatibility for old API.

--- project/src/getToolchain.ts
+++ project/src/getToolchain.ts
@@ -2,5 +2,12 @@ import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec"
 import type {__ModuleExport} from "@anio-software/enkore-private.target-js-toolchain_types"
 
 export function getToolchain(session: EnkoreSessionAPI) {
+	// temporary backwards compat
+	if ("_getToolchain" in session.target) {
+		session.enkore.emitMessage("warning", `USING OLD _getToolchain() API`)
+
+		return (session.target as any)._getToolchain("js") as __ModuleExport
+	}
+
 	return session.target.__getCurrentlyInstalledToolchain() as __ModuleExport
 }
