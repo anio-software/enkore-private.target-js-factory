e387d7c697361584a0cbcc482c1c38f6a4d7b6f8
eXory2024 <exory@anio.software>
2025-07-09 22:41:15 +0200

    Validate protocol.

--- project/embeds/project/_parseEmbedURL.ts
+++ project/embeds/project/_parseEmbedURL.ts
@@ -1,4 +1,5 @@
 import type {EmbedProtocol} from "./EmbedProtocol.ts"
+import {_getEmbedProtocols} from "./_getEmbedProtocols.ts"
 
 export function _parseEmbedURL(embedURL: string): {
 	protocol: EmbedProtocol,
@@ -10,8 +11,14 @@ export function _parseEmbedURL(embedURL: string): {
 		throw new Error(`Invalid embed URL '${embedURL}'.`)
 	}
 
+	const protocol = parts[0] as EmbedProtocol
+
+	if (!_getEmbedProtocols().includes(protocol)) {
+		throw new Error(`Invalid embed protocol '${protocol}'`)
+	}
+
 	return {
-		protocol: parts[0] as EmbedProtocol,
+		protocol,
 		relativePath: parts[1]
 	}
 }
