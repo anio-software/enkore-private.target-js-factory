a20f8fb77c2ffe140ab3f9ef8ceb3786be111a5b
eXory2024 <exory@anio.software>
2025-07-13 18:53:44 +0200

    Add getRequestedEmbedsFromProjectSourceFile and getRequestedEmbedsFromProjectSourceFileRecursive.

---
+++ project/src/targetIntegration/getRequestedEmbedsFromProjectSourceFile.ts
@@ -0,0 +1,51 @@
+import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec";
+import type {APIContext} from "./APIContext.ts";
+import type {RequestedEmbedsFromCodeResult} from "@anio-software/enkore-private.target-js-toolchain_types"
+import {readFileString} from "@anio-software/pkg.node-fs"
+import {getBaseModuleSpecifier} from "#~src/getBaseModuleSpecifier.ts"
+import {getProjectAPIMethodNames} from "#~export/project/getProjectAPIMethodNames.ts"
+import {getToolchain} from "#~src/getToolchain.ts"
+import {getInternalData} from "./getInternalData.ts"
+import path from "node:path"
+
+export async function getRequestedEmbedsFromProjectSourceFile(
+	apiContext: APIContext,
+	session: EnkoreSessionAPI,
+	sourceFilePath: string
+): Promise<RequestedEmbedsFromCodeResult> {
+	const cache = getInternalData(session).requestedEmbedsFileCache
+
+	if (cache.has(sourceFilePath)) {
+		return cache.get(sourceFilePath)!
+	}
+
+	const objectFiles = session.enkore.getCreatedObjectFilesForRelativeSourcePath(
+		sourceFilePath
+	).filter(path => {
+		return path.endsWith(".mjs") || path.endsWith(".js")
+	})
+
+	if (objectFiles.length > 1) {
+		throw new Error(`found more than one object file for path '${sourceFilePath}'`)
+	} else if (!objectFiles.length) {
+		throw new Error(`unable to find object file for path '${sourceFilePath}'`)
+	}
+
+	const sourceCode = await readFileString(
+		path.join(
+			session.project.root, "objects", objectFiles[0]
+		)
+	)
+
+	const result = await getToolchain(session).getRequestedEmbedsFromCode(
+		[`${getBaseModuleSpecifier(apiContext.target)}/project`],
+		getProjectAPIMethodNames().filter(x => {
+			return x.toLowerCase().includes("embed")
+		}),
+		sourceCode
+	)
+
+	cache.set(sourceFilePath, result)
+
+	return result
+}
---
+++ project/src/targetIntegration/getRequestedEmbedsFromProjectSourceFileRecursive.ts
@@ -0,0 +1,39 @@
+import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec";
+import type {APIContext} from "./APIContext.ts";
+import type {RequestedEmbedsFromCodeResult} from "@anio-software/enkore-private.target-js-toolchain_types"
+import {getRequestedEmbedsFromProjectSourceFile} from "./getRequestedEmbedsFromProjectSourceFile.ts"
+import {getModuleGuarded} from "./getModuleGuarded.ts"
+import {getInternalData} from "./getInternalData.ts"
+
+export async function getRequestedEmbedsFromProjectSourceFileRecursive(
+	apiContext: APIContext,
+	session: EnkoreSessionAPI,
+	sourceFilePath: string
+): Promise<RequestedEmbedsFromCodeResult[]> {
+	const ret: RequestedEmbedsFromCodeResult[] = []
+
+	const mod = getModuleGuarded(
+		getInternalData(session).myTSProgram,
+		`build/${sourceFilePath}`
+	)
+
+	const filesToAnalyze: Set<string> = new Set()
+
+	for (const moduleSpecifier of mod.referencedModuleSpecifiers) {
+		if (moduleSpecifier.startsWith("build/")) {
+			filesToAnalyze.add(moduleSpecifier.slice("build/".length))
+		}
+	}
+
+	filesToAnalyze.add(mod.filePath.slice("build/".length))
+
+	for (const [filePath] of filesToAnalyze.entries()) {
+		ret.push(
+			await getRequestedEmbedsFromProjectSourceFile(
+				apiContext, session, filePath
+			)
+		)
+	}
+
+	return ret
+}
