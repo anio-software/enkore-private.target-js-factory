05aa6fe7894a9d4bb1523f7cf34c9f21753d034d
eXory2024 <exory@anio.software>
2025-07-13 19:51:29 +0200

    Using createEntity makes the bundling fail because it contains 'await' statements.
    This is most likely because @anio-software/pkg.node-fs contains old runtime init code.
    
    This definitely needs to be fixed.

--- project/embeds/project/_getInitialGlobalState.ts
+++ project/embeds/project/_getInitialGlobalState.ts
@@ -1,12 +1,18 @@
-import {createEntity} from "@anio-software/enkore-private.spec"
+import type {EnkoreJSRuntimeGlobalState} from "@anio-software/enkore-private.spec"
 
-export function _getInitialGlobalState() {
-	return createEntity("EnkoreJSRuntimeGlobalState", 0, 0, {
+export function _getInitialGlobalState(): EnkoreJSRuntimeGlobalState {
+	return {
+		entityKind: "EnkoreJSRuntimeGlobalState",
+		entityMajorVersion: 0,
+		entityRevision: 0,
+		entityCreatedBy: null,
+
 		immutable: {
 			embeds: new Map()
 		},
+
 		mutable: {
 			embedResourceURLs: new Map()
 		}
-	})
+	}
 }
