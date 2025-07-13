155305ab4f6a76a8de75b227d7026c5f19565f2b
eXory2024 <exory@anio.software>
2025-07-13 19:40:35 +0200

    We need to bundle runtime init code.

--- project/src/targetIntegration/generateRuntimeInitCode.ts
+++ project/src/targetIntegration/generateRuntimeInitCode.ts
@@ -2,11 +2,19 @@ import type {APIContext} from "./APIContext.ts"
 import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec"
 import type {ProjectAPIContext} from "#~embeds/project/ProjectAPIContext.ts"
 import type {EntryPoint} from "./InternalData.ts"
+import {getToolchain} from "#~src/getToolchain.ts"
 
-function iife(code: string) {
-	return `
-;(function() {${code}})();
-`
+async function bundle(
+	session: EnkoreSessionAPI,
+	code: string
+) {
+	const toolchain = getToolchain(session)
+
+	return await toolchain.jsBundler(
+		session.project.root, code, {
+			outputFormat: "iife"
+		}
+	)
 }
 
 export async function generateRuntimeInitCode(
@@ -23,5 +31,5 @@ export async function generateRuntimeInitCode(
 
 	let code = ``
 
-	return iife(code)
+	return await bundle(session, code)
 }
