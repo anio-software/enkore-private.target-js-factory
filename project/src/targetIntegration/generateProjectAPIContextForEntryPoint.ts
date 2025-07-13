95326e00b4773bdc9b315df23edd7e7a2a526dc2
eXory2024 <exory@anio.software>
2025-07-13 18:52:05 +0200

    Project API context doesn't really need to be trimmed with the new way we are handling embeds.
    Also the printing of the embeds that are going to be used for each entry point will be logged somewhere else.

--- project/src/targetIntegration/generateProjectAPIContextForEntryPoint.ts
+++ project/src/targetIntegration/generateProjectAPIContextForEntryPoint.ts
@@ -13,18 +13,6 @@ export async function generateProjectAPIContextForEntryPoint(
 
 	const bundlerProjectContext = {...projectContext}
 
-	// trim context here
-
-	let message = `entry point '${entryPointPath}' will contain the following embeds:\n`
-
-	for (const [embedPath] of bundlerProjectContext._projectEmbedFileMapRemoveMeInBundle!.entries()) {
-		message += ` - ${embedPath}\n`
-	}
-
-	if (bundlerProjectContext._projectEmbedFileMapRemoveMeInBundle!.size) {
-		session.enkore.emitMessage("info", message.slice(0, -1))
-	}
-
 	delete bundlerProjectContext._projectEmbedFileMapRemoveMeInBundle
 
 	return bundlerProjectContext
