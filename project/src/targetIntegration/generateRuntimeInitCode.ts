84095cb0052d8e77eeaad07e39e22ed3403d0b2e
eXory2024 <exory@anio.software>
2025-07-13 19:43:54 +0200

    Update generateRuntimeInitCode.

--- project/src/targetIntegration/generateRuntimeInitCode.ts
+++ project/src/targetIntegration/generateRuntimeInitCode.ts
@@ -3,6 +3,8 @@ import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec"
 import type {ProjectAPIContext} from "#~embeds/project/ProjectAPIContext.ts"
 import type {EntryPoint} from "./InternalData.ts"
 import {getToolchain} from "#~src/getToolchain.ts"
+import {parseEmbedURL} from "@anio-software/enkore-private.spec/utils"
+import {createEntity} from "@anio-software/enkore-private.spec"
 
 async function bundle(
 	session: EnkoreSessionAPI,
@@ -29,7 +31,34 @@ export async function generateRuntimeInitCode(
 		return ""
 	}
 
+	const {packageJSON} = session.project
+
 	let code = ``
+
+	code += `const embedsSymbol = Symbol.for("@anio-software/enkore/global/embeds");\n`
+
+	code += `if (!(embedSymbol in globalThis)) {\n`
+	code += `\tglobalThis[embedSymbol] = new Map();\n`
+	code += `}\n`
+
+	code += `const embedsMap = globalThis[embedSymbol];\n`
+
+	for (const [embedURL, {createResourceAtRuntime}] of entryPoint.embeds.entries()) {
+		const embed = projectContext._projectEmbedFileMapRemoveMeInBundle!.get(embedURL)!
+		const {protocol, path: sourceFilePath} = parseEmbedURL(embedURL)
+		const globalIdentifier = `${packageJSON.name}/v${packageJSON.version}/${protocol}/${sourceFilePath}`
+
+		const data = createEntity("EnkoreJSRuntimeEmbeddedFile", 0, 0, {
+			createResourceAtRuntimeInit: createResourceAtRuntime,
+			sourceFilePath,
+			url: embedURL,
+			data: embed.data
+		})
+
+		code += `if (!embedsMap.has("${globalIdentifier}")) {\n`
+		code += `\tembedsMap.set("${globalIdentifier}", ${JSON.stringify(data)})\n`
+		code += `}\n`
+	}
 
 	return await bundle(session, code)
 }
