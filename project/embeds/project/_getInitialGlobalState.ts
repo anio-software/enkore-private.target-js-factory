1d9f91534416e5b053107d9ac52ddfc8e17d449d
eXory2024 <exory@anio.software>
2025-07-13 19:41:50 +0200

    Add _getInitialGlobalState, we'll need it in the runtime init code.

---
+++ project/embeds/project/_getInitialGlobalState.ts
@@ -0,0 +1,12 @@
+import {createEntity} from "@anio-software/enkore-private.spec"
+
+export function _getInitialGlobalState() {
+	return createEntity("EnkoreJSRuntimeGlobalState", 0, 0, {
+		immutable: {
+			embeds: new Map()
+		},
+		mutable: {
+			embedResourceURLs: new Map()
+		}
+	})
+}
