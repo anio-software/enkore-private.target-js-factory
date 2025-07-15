5220d5a64212ef620873adcc663c22cbd32a903e
eXory2024 <exory@anio.software>
2025-07-15 06:20:48 +0200

    Comment somehow caused issues.

--- project/embeds/projectAPI/moduleTemplate.ts
+++ project/embeds/projectAPI/moduleTemplate.ts
@@ -16,7 +16,8 @@ import {
 	getEmbedData,
 	getGlobalState,
 	translateEmbedURLToGlobalIdentifier
-} from "js-runtime-helpers/v0" // <-- module specifier will be fixed for rollup version
+} from "js-runtime-helpers/v0"
+// ^^^^^^^^^^^^^^^^^^^^^^^ module specifier will be fixed for rollup version
 
 function __getContext(): any {
 	return "%%CONTEXT_DATA%%"
