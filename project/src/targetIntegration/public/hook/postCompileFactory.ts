a08fcf64c01341171688f4e93541d1abf6b8b746
eXory2024 <exory@anio.software>
2025-07-16 00:01:53 +0200

    Set flag when postCompile was called by enkore.

--- project/src/targetIntegration/public/hook/postCompileFactory.ts
+++ project/src/targetIntegration/public/hook/postCompileFactory.ts
@@ -1,10 +1,12 @@
 import type {API} from "#~src/targetIntegration/API.ts"
 import type {APIContext} from "#~src/targetIntegration/APIContext.ts"
+import {getInternalData} from "#~src/targetIntegration/getInternalData.ts"
 
 const impl: API["hook"]["postCompile"] = async function(
 	this: APIContext, session
 ) {
-
+	// todo: remove me in the future
+	getInternalData(session)._backwardsCompatPostCompileHook.needsManualInvocation = false
 }
 
 export function postCompileFactory(context: APIContext) {
