f6bc712b7ac2ebb5bc604f9cb662a2beae73229e
eXory2024 <exory@anio.software>
2025-07-19 01:02:11 +0200

    Delegate code creation to _generateTryFactoryCode.

---
+++ project/src/autogenerate/generateFactoryInstantiationFile/_generateTryFactoryCode.ts
@@ -0,0 +1,13 @@
+import type {AutogenerateAPIContext} from "../AutogenerateAPIContext.ts"
+import type {EnkoreSessionAPI} from "@anio-software/enkore-private.spec"
+import type {Options} from "./Options.ts"
+import type {Variant} from "./Variant.ts"
+
+export function _generateTryFactoryCode(
+	context: AutogenerateAPIContext,
+	session: EnkoreSessionAPI,
+	options: Options,
+	variant: Variant
+) {
+	return ""
+}
